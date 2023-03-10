926deb36312d34e2c26a8ef259c8bfe8608866c9
Zeng <Zeming.Zeng@harman.com>
2023-03-10 10:45:41 +0800

    [JavaStudy]
    java study:20230310

--- JavaStudy/Main.java
+++ JavaStudy/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello vscode!");
+    System.out.println("Hello vscode!!");
     JavaStudy javaStudy = new JavaStudy();
     javaStudy.study();
   }
